6547970c7fd93a83216a690de01ff3d963ab4787
JVCAv1 <joao.assumpo@gmail.com>
2022-10-28 16:23:34 -0300

    Display for Board, Cell and State... just in case

--- src/lib.rs
+++ src/lib.rs
@@ -28,4 +28,45 @@ impl Board {
 			cells: Array2::from_elem((3, 3), Cell { state: State::Nil }),
         }
     }
+
+	pub fn get_cell_state(&self, x: usize, y: usize) -> String {
+		format!("{}", self.cells[[x, y]].state)
+	}
+}
+
+
+impl std::fmt::Display for Board {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		for row in self.cells.outer_iter() {
+			for cell in row {
+				match cell.state {
+					State::X => write!(f, "X")?,
+					State::O => write!(f, "O")?,
+					State::Nil => write!(f, " ")?,
+				}
+			}
+			writeln!(f)?;
+		}
+		Ok(())
+	}
+}
+
+impl std::fmt::Display for Cell {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match self.state {
+			State::X => write!(f, "X"),
+			State::O => write!(f, "O"),
+			State::Nil => write!(f, " "),
+		}
+	}
+}
+
+impl std::fmt::Display for State {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match self {
+			State::X => write!(f, "X"),
+			State::O => write!(f, "O"),
+			State::Nil => write!(f, " "),
+		}
+	}
 }
