7378478901c0ec0aa6551782e56944ee2e6dd5a5
JVCAv1 <joao.assumpo@gmail.com>
2022-10-28 15:56:33 -0300

    Board types and new() constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub enum Player{
-	X,
-	O,
+#[derive(Copy, Clone)]
+enum State {
+    X,
+    O,
+    Nil,
 }
 
+#[derive(Copy, Clone)]
+struct Cell {
+    state: State,
+}
+
+pub struct Board {
+    cells: [[[Cell; 3]; 3]; 3],
+}
+
+impl Default for Board {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board {
+            cells: [[[Cell { state: State::Nil }; 3]; 3]; 3],
+        }
+    }
+}
